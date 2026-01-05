961f0e2e894acb217c79cf4fbbc1008959ee3e82
AbdulMoid-20 <abdulmoidsiddiqui2008@gmail.com>
2026-01-05 09:10:52 +0500

    first commit

--- script.js
+++ script.js
@@ -162,3 +162,12 @@ function openEmail() {
     
 }
 
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
